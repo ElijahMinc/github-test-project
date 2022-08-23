3a02d2534f207877388ce4ea8fef29a1b9d0ed23
ElijahMinc <gvidar89@gmail.com>
2022-08-23 13:59:17 +0300

    Added ingnore .env in .gitignore file, added a 401 error interceptor to axios, as well as a toast call to notify the user of this error

--- src/http/axios.ts
+++ src/http/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { errorToast } from "../components/Common/Toast/actions";
 
 const $BaseApi = axios.create({
   baseURL: process.env.REACT_APP_API_URL ?? "https://api.github.com",
@@ -7,5 +8,24 @@ const $BaseApi = axios.create({
     Authorization: `token ${process.env.REACT_APP_PERSONAL_TOKEN}`,
   },
 });
+
+$BaseApi.interceptors.response.use(
+  (config) => config,
+  async (err) => {
+    const originalRequest = err.config;
+
+    if (
+      err.response.status === 401 &&
+      originalRequest &&
+      !originalRequest._isRetry
+    ) {
+      originalRequest._isRetry = true;
+
+      errorToast(
+        "Appliaction need YOUR Access-token for increase the request limit"
+      );
+    }
+  }
+);
 
 export { $BaseApi };
