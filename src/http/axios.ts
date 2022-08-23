7fa698cf2a79a1318a6828633cf2eeb98d8d54fb
ElijahMinc <gvidar89@gmail.com>
2022-08-23 13:37:29 +0300

    Authorization token fix for extended data retrieval limit

--- src/http/axios.ts
+++ src/http/axios.ts
@@ -4,7 +4,7 @@ const $BaseApi = axios.create({
   baseURL: process.env.REACT_APP_API_URL ?? "https://api.github.com",
   headers: {
     Accept: "application/vnd.github+json",
-    Authorization: `token ${process.env.REACT_APP_PERSONAL_TOKEN}`,
+    Authorization: `token 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`,
   },
 });
 
