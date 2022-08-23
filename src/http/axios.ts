97514cbc6c76bc4eeda78e935a4fbd854b92755b
ElijahMinc <gvidar89@gmail.com>
2022-08-23 15:17:50 +0300

    Removed access token in request

--- src/http/axios.ts
+++ src/http/axios.ts
@@ -5,7 +5,7 @@ const $BaseApi = axios.create({
   baseURL: process.env.REACT_APP_API_URL ?? "https://api.github.com",
   headers: {
     Accept: "application/vnd.github+json",
-    Authorization: `token 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`,
+    Authorization: `token ${process.env.REACT_APP_PERSONAL_TOKEN}`,
   },
 });
 
