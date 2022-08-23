066626b664f85f310c0c1d6f23a8ed281d659e49
ElijahMinc <gvidar89@gmail.com>
2022-08-23 13:21:53 +0300

    generated and added access token for increase API rate limit

--- src/http/axios.ts
+++ src/http/axios.ts
@@ -4,8 +4,11 @@ const $BaseApi = axios.create({
   baseURL: process.env.REACT_APP_API_URL ?? "https://api.github.com",
   headers: {
     Accept: "application/vnd.github+json",
-    // Authorization: `token ${process.env.REACT_APP_PERSONAL_TOKEN}`,
-    // 
+    Authorization: `token ${
+      process.env.REACT_APP_PERSONAL_TOKEN ??
+      "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"
+    }`,
+    //
   },
 });
 
