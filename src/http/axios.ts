4099e4a27786a08a83a19d5ea7d394ee910530f4
ElijahMinc <gvidar89@gmail.com>
2022-08-23 13:48:17 +0300

    Removed the open token in the instance axios request header, letting the developer insert their own token

--- src/http/axios.ts
+++ src/http/axios.ts
@@ -4,7 +4,7 @@ const $BaseApi = axios.create({
   baseURL: process.env.REACT_APP_API_URL ?? "https://api.github.com",
   headers: {
     Accept: "application/vnd.github+json",
-    Authorization: `token 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`,
+    Authorization: `token ${process.env.REACT_APP_PERSONAL_TOKEN}`,
   },
 });
 
